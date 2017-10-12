a088a3aeebe13d32932f7af1d7fc07bf235dd614
Joseph Turk <joseph.r.turk@gmail.com>
2017-10-12 13:05:36 -0500

    Add VueFire

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueFire from 'vuefire'
 import App from './App'
 import router from './router'
 require('reset-css/reset.css')
 
 Vue.config.productionTip = false
+
+Vue.use(VueFire)
 
 /* eslint-disable no-new */
 new Vue({
